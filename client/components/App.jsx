708c9be7e80135b8ef011f1bb6e3494736682b68
Hamish Smith <44103834+hamish-w-smith@users.noreply.github.com>
2019-06-26 14:36:08 +1200

    npm install react google maps

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,36 +1,36 @@
 import React from 'react'
 
+
 import { getRainLast6Hours } from '../apis/gwrcRainLast6Hours'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      rainLast6Hours: {}
-    }
+    // this.state = {
+    //   rainLast6Hours: {}
+    // }
   }
 
-  componentDidMount() {
-    this.getRainLast6HoursData()
-  }
+  // componentDidMount() {
+  //   this.getRainLast6HoursData()
+  // }
 
-  getRainLast6HoursData = () => {
-    getRainLast6Hours(issId)
-      .then(satellite => {
-        this.setState({
-          satellite: satellite
-        })
-      })
-  }
+  // getRainLast6HoursData = () => {
+  //   getRainLast6Hours(issId)
+  //     .then(satellite => {
+  //       this.setState({
+  //         satellite: satellite
+  //       })
+  //     })
+  // }
 
   render() {
     return (
       <>
-        <h1>Where has it rained?</h1>
-        Lat: {this.state.satellite.latitude}<br />
-        Lng: {this.state.satellite.longitude}<br />
-        <button onClick={this.getSatelliteData}>Refresh</button>
+        <h1>A map</h1>
+
+
       </>
     )
   }
